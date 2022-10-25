6e37bdb3573c047faf1afd3b76bee6d25d320e81
Artyom7777 <iartiq@gmail.com>
2022-10-25 06:51:14 +0300

    Done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,23 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        // integer variable to be converted to char
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+
+        //use typecasting to convert int to char in Java
+        char e = (char) c;
+        char q = (char) h;
+        char z = (char) a;
+        char x = (char) r;
+
+        //print the character
+        System.out.println(e);
+        System.out.println(q);
+        System.out.println(z);
+        System.out.println(x);
+
     }
 }
